397f927758ec5b1ae2edaa2a3754824a58ca421b
Dmitriy <d.hodikin@yandex.ru>
2021-10-05 21:48:15 +0300

    feat(gui): Add gui main window

--- src/gui.py
+++ src/gui.py
@@ -4,11 +4,36 @@ Game user interface.
 Docs: https://wiki.python.org/moin/PyQt
 """
 
-from PyQt5.QtWidgets import QApplication, QWidget
+import sys
+from PyQt5.QtWidgets import (QWidget, QLineEdit,
+                             QInputDialog, QApplication)
 
-app = QApplication([])
-window = QWidget()
-window.setGeometry(500, 200, 600, 300)
-window.setWindowTitle("Blotto Game")
-window.show()
-app.exec()
+
+class MainWindow(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.init_gui()
+
+    def init_gui(self):
+        position = QLineEdit(self)
+        position.move(20, 20)
+        position.resize(150, 30)
+
+        self.setGeometry(500, 300, 600, 300)
+        self.setWindowTitle('Blotto Game')
+        self.show()
+
+    def show_dialog(self):
+        text, ok = QInputDialog.getText(self, 'Input Dialog',
+                                        'Enter your name:')
+
+        if ok:
+            self.le.setText(str(text))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MainWindow()
+    sys.exit(app.exec_())
