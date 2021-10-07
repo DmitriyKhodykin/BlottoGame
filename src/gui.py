825d521cf502df2c6856f1fa226eb1ddf3578131
Dmitriy_Khodykin <d.hodikin@yandex.ru>
2021-10-07 11:54:47 +0300

    fix(gui): Add __init__ fields

--- src/gui.py
+++ src/gui.py
@@ -15,6 +15,12 @@ class UserWindow(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.position1 = None
+        self.position2 = None
+        self.position3 = None
+        self.position4 = None
+        self.position5 = None
+        self.button = None
         self.init_gui()
 
     def init_gui(self):
@@ -36,48 +42,48 @@ class UserWindow(QWidget):
         textbox.move(left_border, 10)
         textbox.resize(500, 100)
 
-        position1 = QLineEdit(self)
-        position1.setFont(QFont('Arial', 25))
-        position1.setAlignment(Qt.AlignCenter)
-        position1.move(left_border, top_border)
-        position1.resize(position_width, position_height)
+        self.position1 = QLineEdit(self)
+        self.position1.setFont(QFont('Arial', 25))
+        self.position1.setAlignment(Qt.AlignCenter)
+        self.position1.move(left_border, top_border)
+        self.position1.resize(position_width, position_height)
 
-        position2 = QLineEdit(self)
-        position2.setFont(QFont('Arial', 25))
-        position2.setAlignment(Qt.AlignCenter)
-        position2.move(left_border + position_width, top_border)
-        position2.resize(position_width, position_height)
+        self.position2 = QLineEdit(self)
+        self.position2.setFont(QFont('Arial', 25))
+        self.position2.setAlignment(Qt.AlignCenter)
+        self.position2.move(left_border + position_width, top_border)
+        self.position2.resize(position_width, position_height)
 
-        position3 = QLineEdit(self)
-        position3.setFont(QFont('Arial', 25))
-        position3.setAlignment(Qt.AlignCenter)
-        position3.move(left_border + position_width * 2, top_border)
-        position3.resize(position_width, position_height)
+        self.position3 = QLineEdit(self)
+        self.position3.setFont(QFont('Arial', 25))
+        self.position3.setAlignment(Qt.AlignCenter)
+        self.position3.move(left_border + position_width * 2, top_border)
+        self.position3.resize(position_width, position_height)
 
-        position4 = QLineEdit(self)
-        position4.setFont(QFont('Arial', 25))
-        position4.setAlignment(Qt.AlignCenter)
-        position4.move(left_border + position_width * 3, top_border)
-        position4.resize(position_width, position_height)
+        self.position4 = QLineEdit(self)
+        self.position4.setFont(QFont('Arial', 25))
+        self.position4.setAlignment(Qt.AlignCenter)
+        self.position4.move(left_border + position_width * 3, top_border)
+        self.position4.resize(position_width, position_height)
 
-        position5 = QLineEdit(self)
-        position5.setFont(QFont('Arial', 25))
-        position5.setAlignment(Qt.AlignCenter)
-        position5.move(left_border + position_width * 4, top_border)
-        position5.resize(position_width, position_height)
+        self.position5 = QLineEdit(self)
+        self.position5.setFont(QFont('Arial', 25))
+        self.position5.setAlignment(Qt.AlignCenter)
+        self.position5.move(left_border + position_width * 4, top_border)
+        self.position5.resize(position_width, position_height)
 
-        button = QPushButton('Make a guess', self)
-        button.setFont(QFont('Arial', 11))
-        button.clicked.connect(self.click_method)
-        button.move(190, 250)
-        button.resize(200, 32)
+        self.button = QPushButton('Make a guess', self)
+        self.button.setFont(QFont('Arial', 11))
+        self.button.clicked.connect(self.click_method)
+        self.button.move(190, 250)
+        self.button.resize(200, 32)
 
         self.setGeometry(500, 300, 600, 300)
         self.setWindowTitle('Blotto Game')
         self.show()
 
     def click_method(self):
-        print('Clicked')
+        print('Position 1 number:', self.position1.text())
 
 
 if __name__ == '__main__':
