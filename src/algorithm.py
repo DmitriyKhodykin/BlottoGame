0703b49cae7f73731e51194cc396d377e3b6f28a
Dmitriy <d.hodikin@yandex.ru>
2021-10-07 20:44:59 +0300

    feat(algorithm): Add blotto func

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,23 +1,37 @@
 """
 Module with the implementation of Blotto Game theory.
 """
-x = [20, 19, 22, 19, 20]  # Gamer
-y = [20, 19, 21, 20, 20]  # AI
 
-print("X:", sum(x))
-print("Y:", sum(x))
 
-res = 0
-for i in range(len(x)):
-    r = x[i] - y[i]
-    if r < 0:
-        res = res + 1
+def blotto_algorithm(user_guess: list):
+    """
+    Compare positions in pairs and calculate the result.
+    :return: None
+    """
+    computer = [20, 20, 17, 21, 22]  # AI
 
-print("RES:", res)
+    print("human:", user_guess, "SUM:", sum(user_guess))
+    print("computer:", computer, "SUM:", sum(computer))
 
-if res > 2:
-    print("You are lose")
-elif res == 0:
-    print("Draw game")
-else:
-    print("You are win")
+    res = 0
+    for i in range(len(user_guess)):
+        r = user_guess[i] - computer[i]
+        if r < 0:
+            res = res + 1
+        elif r == 0:
+            pass
+        elif r > 0:
+            res = res - 1
+
+    print("RES:", res)
+
+    if res > 0:
+        print("You are lose")
+    elif res == 0:
+        print("Draw game")
+    else:
+        print("You are win")
+
+
+if __name__ == '__main__':
+    blotto_algorithm([20, 20, 20, 20, 20])
