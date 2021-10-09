a6e4d40386f94056e71e0b63161a4bf6830dd73d
Dmitriy <d.hodikin@yandex.ru>
2021-10-09 15:56:47 +0300

    feat(algorithm): Add computer random choice

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,6 +1,7 @@
 """
 Module with the implementation of Blotto Game theory.
 """
+
 import numpy
 
 
@@ -9,32 +10,40 @@ def blotto_algorithm(user_guess: list):
     Compare positions in pairs and calculate the result.
     :return: None
     """
-    computer = [20, 20, 17, 21, 22]  # AI
-    c1 = numpy.random.normal(20, 5, 5)
-    c1 = numpy.round(c1, 0)
-
+    # Random computer guess
+    random_list = numpy.random.randint(0, 100, 5)
+    computer_guess = []
+    for i in random_list:
+        sum_random = sum(random_list)
+        random_choice = int(round(i / sum_random * 100, 0))
+        computer_guess.append(random_choice)
     print("human:", user_guess, "SUM:", sum(user_guess))
-    print("computer:", c1, "SUM:", sum(c1))
+    print("computer:", computer_guess, "SUM:", sum(computer_guess))
 
-    res = 0
+    # Count the result
+    result = 0
     for i in range(len(user_guess)):
-        r = user_guess[i] - computer[i]
-        if r < 0:
-            res = res + 1
-        elif r == 0:
+        temp_result = user_guess[i] - computer_guess[i]
+        if temp_result < 0:
+            result = result + 1
+        elif temp_result == 0:
             pass
-        elif r > 0:
-            res = res - 1
+        elif temp_result > 0:
+            result = result - 1
 
-    print("RES:", res)
+    print("Computer - Human Sum:", result)
 
-    if res > 0:
-        print("You are lose")
-    elif res == 0:
-        print("Draw game")
+    # Refereeing
+    if result > 0:
+        result_message = 'You are lose'
+    elif result == 0:
+        result_message = 'Draw game'
     else:
-        print("You are win")
+        result_message = 'You are win'
+    print(result_message)
+
+    return computer_guess, result_message
 
 
 if __name__ == '__main__':
-    blotto_algorithm([20, 20, 20, 20, 20])
+    blotto_algorithm([22, 22, 22, 20, 14])
