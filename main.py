9e0dfb27dd225b5a7f1a747448b3b990bde70557
Dmitriy_Khodykin <d.hodikin@yandex.ru>
2021-10-15 10:31:42 +0300

    refactor(gui): Add all fields with result in ResultWindow Class

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ Main module.
 """
 
 from PyQt5.QtWidgets import QApplication
-from src.gui import Window
+from src.gui import UserWindow
 
 if __name__ == '__main__':
     app = QApplication([])
-    window = Window()
+    window = UserWindow()
     app.exec_()
--- src/gui.py
+++ src/gui.py
@@ -1,8 +1,7 @@
 """
 Game user interface.
 
-Docs: https://doc.qt.io/qtforpython/,
-      https://doc.qt.io/qt-5/qtexamplesandtutorials.html
+Docs: https://doc.qt.io/qtforpython/
 """
 
 from PyQt5.QtCore import Qt
@@ -13,18 +12,18 @@ from PyQt5.QtWidgets import (QWidget, QLineEdit,
 from src.algorithm import blotto_algorithm
 
 
-class Window(QWidget):
+class UserWindow(QWidget):
     """
-    User guess Window.
+    User's Resource value input window.
     """
     def __init__(self):
         super().__init__()
         self.textbox = None  # Textbox with game's rules
-        self.position6 = None  # User's positions are 6-10
-        self.position7 = None
-        self.position8 = None
-        self.position9 = None
-        self.position10 = None
+        self.position_one = None
+        self.position_two = None
+        self.position_three = None
+        self.position_four = None
+        self.position_five = None
         self.button = None
         self.left_border = 45  # Border for all elements
         self.top_border_human = 130  # Border for human positions
@@ -47,20 +46,20 @@ class Window(QWidget):
         self.textbox.move(self.left_border, 10)
 
         # User positions
-        self.position6 = Position(self)
-        self.position6.move(self.left_border, self.top_border_human)
+        self.position_one = Position(self)
+        self.position_one.move(self.left_border, self.top_border_human)
 
-        self.position7 = Position(self)
-        self.position7.move(self.left_border + 100, self.top_border_human)
+        self.position_two = Position(self)
+        self.position_two.move(self.left_border + 100, self.top_border_human)
 
-        self.position8 = Position(self)
-        self.position8.move(self.left_border + 200, self.top_border_human)
+        self.position_three = Position(self)
+        self.position_three.move(self.left_border + 200, self.top_border_human)
 
-        self.position9 = Position(self)
-        self.position9.move(self.left_border + 300, self.top_border_human)
+        self.position_four = Position(self)
+        self.position_four.move(self.left_border + 300, self.top_border_human)
 
-        self.position10 = Position(self)
-        self.position10.move(self.left_border + 400, self.top_border_human)
+        self.position_five = Position(self)
+        self.position_five.move(self.left_border + 400, self.top_border_human)
 
         # Ok Button
         self.button = OkButton('Make a guess', self)
@@ -76,11 +75,11 @@ class Window(QWidget):
         Get list with user positions.
         :return: List like [20, 20, 20, 20, 20]
         """
-        position_number_6 = int(self.position6.text())
-        position_number_7 = int(self.position7.text())
-        position_number_8 = int(self.position8.text())
-        position_number_9 = int(self.position9.text())
-        position_number_10 = int(self.position10.text())
+        position_number_6 = int(self.position_one.text())
+        position_number_7 = int(self.position_two.text())
+        position_number_8 = int(self.position_three.text())
+        position_number_9 = int(self.position_four.text())
+        position_number_10 = int(self.position_five.text())
         user_guess = [position_number_6,
                       position_number_7,
                       position_number_8,
@@ -111,18 +110,20 @@ class ResultWindow(QWidget):
                  computer_result_box,
                  result_message_box):
         super().__init__()
-        self.user_result_box = user_result_box
-        self.computer_result_box = computer_result_box
+        self.user_guess = user_result_box
+        self.computer_guess = computer_result_box
         self.result_message_box = result_message_box
         self.textbox = None
         self.init_gui()
 
     def init_gui(self):
-        result = f'Result: {self.result_message_box}'
+        result = f'Result: {self.result_message_box} \n' \
+                 f'Computer guess:  {self.computer_guess} \n' \
+                 f'User guess: {self.user_guess}'
         self.textbox = TextBox(result, self)
 
         # Window Geometry
-        self.setGeometry(500, 300, 600, 300)
+        self.setGeometry(550, 350, 600, 300)
         self.setWindowTitle('Blotto Game Result')
         self.show()
 
