9e3c228d7b25a45eaf440a6f6a6285c779ad4bba
Dmitriy_Khodykin <d.hodikin@yandex.ru>
2021-10-20 14:30:06 +0300

    feat(caching): Add CacheResult class

--- src/caching.py
+++ src/caching.py
@@ -10,8 +10,15 @@ class CacheResult:
     def __init__(self):
         self.client = base.Client(('localhost', 11211))
 
-    def set_result(self):
-        self.client.set('some_key', 'some value')
+    def set_result(self, winner: str):
+        self.client.set(winner, 1)
 
     def get_result(self):
-        self.client.get('some_key')
+        user_count = self.client.get('user')
+        comp_count = self.client.get('comp')
+        print('User Score:', user_count, 'Computer Score:', comp_count)
+
+
+if __name__ == '__main__':
+    cache = CacheResult()
+    cache.set_result('user')
