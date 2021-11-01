93b4c21afd16ab926023c77d261cb28b6e57b74c
Dmitriy_Khodykin <d.hodikin@yandex.ru>
2021-11-01 15:00:45 +0300

    refactor(gui): Add user positions loop for grid instead five vars

--- src/gui.py
+++ src/gui.py
@@ -8,7 +8,7 @@ Docs: https://doc.qt.io/qtforpython/
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import (QWidget, QLineEdit, QPushButton,
-                             QLabel, QHBoxLayout, QVBoxLayout)
+                             QLabel, QHBoxLayout, QVBoxLayout, QGridLayout)
 
 from src.algorithm import blotto_algorithm
 
@@ -22,16 +22,10 @@ class UserWindow(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.textbox = None  # Textbox with game's rules
-        self.position_one = None
-        self.position_two = None
-        self.position_three = None
-        self.position_four = None
-        self.position_five = None
+        self.textbox = None
+        self.user_positions_grid = None
         self.guess_button = None
         self.clear_button = None
-        self.left_border = 45  # Border for all elements
-        self.top_border_human = 130  # Border for human positions
         self.result_window = None
         self.init_gui()
 
@@ -40,7 +34,7 @@ class UserWindow(QWidget):
         Basic Window layout.
         :return: None
         """
-
+        # WIDGETS:
         # Game rules
         rules = "Rules. You have 100 resource units. " \
                 "Distribute resources by " \
@@ -50,38 +44,30 @@ class UserWindow(QWidget):
         self.textbox = TextBox(rules, self)
 
         # User positions
-        self.position_one = Position(self)
-        self.position_two = Position(self)
-        self.position_three = Position(self)
-        self.position_four = Position(self)
-        self.position_five = Position(self)
+        self.user_positions_grid = QGridLayout()
+        for i in range(5):
+            position = Position(self)
+            self.user_positions_grid.addWidget(position, 0, i)
 
-        # BUTTONS:
         # Main button for user guess
         self.guess_button = OkButton('Make a guess', self)
         self.guess_button.clicked.connect(self.click_method)
 
         # Button to clear user input
         self.clear_button = OkButton('Clear', self)
+        # TODO: Make clear func .clicked.connect()
 
-        # Window layouts
+        # WINDOW LAYOUTS:
         vertical_layouts = QVBoxLayout(self)
         vertical_layouts.setSpacing(10)
         horizontal_layouts_rules = QHBoxLayout(self)
-        horizontal_layouts_positions = QHBoxLayout(self)
-        horizontal_layouts_positions.setContentsMargins(0, 0, 0, 110)
+        self.user_positions_grid.setContentsMargins(0, 0, 0, 110)
         horizontal_layouts_buttons = QHBoxLayout(self)
 
         # The first row - game rules
         horizontal_layouts_rules.addWidget(self.textbox)
 
-        # The second row - user positions
-        horizontal_layouts_positions.addWidget(self.position_one)
-        horizontal_layouts_positions.addWidget(self.position_two)
-        horizontal_layouts_positions.addWidget(self.position_three)
-        horizontal_layouts_positions.addWidget(self.position_four)
-        horizontal_layouts_positions.addWidget(self.position_five)
-
+        # The second row - user_positions_grid
         # The third row - buttons
         horizontal_layouts_buttons.addWidget(self.guess_button)
         horizontal_layouts_buttons.addWidget(self.clear_button)
@@ -89,9 +75,10 @@ class UserWindow(QWidget):
         # Put the horizontal layouts in the vertical layouts
         vertical_layouts.addStretch(1)
         vertical_layouts.addLayout(horizontal_layouts_rules)
-        vertical_layouts.addLayout(horizontal_layouts_positions)
+        vertical_layouts.addLayout(self.user_positions_grid)
         vertical_layouts.addLayout(horizontal_layouts_buttons)
 
+        # Final layout
         self.setLayout(vertical_layouts)
 
         # Window Geometry
@@ -104,16 +91,14 @@ class UserWindow(QWidget):
         Get list with user positions.
         :return: List like [20, 20, 20, 20, 20]
         """
-        position_number_6 = int(self.position_one.text())
-        position_number_7 = int(self.position_two.text())
-        position_number_8 = int(self.position_three.text())
-        position_number_9 = int(self.position_four.text())
-        position_number_10 = int(self.position_five.text())
-        user_guess = [position_number_6,
-                      position_number_7,
-                      position_number_8,
-                      position_number_9,
-                      position_number_10]
+        user_guess: list = []
+
+        for i in range(5):
+            widget_of_grid = self.user_positions_grid.itemAtPosition(0, i)
+            widget = widget_of_grid.widget()
+            num_of_widget = int(widget.text())
+            user_guess.append(num_of_widget)
+
         return user_guess
 
     def click_method(self):
